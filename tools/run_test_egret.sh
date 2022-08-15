01d7ff04bbac6b92833be53d999a46a515c01429
Sunrise723 <sunrise723@163.com>
2022-08-15 15:52:37 +0800

    solve conflicts

--- tools/run_test_egret.sh
+++ tools/run_test_egret.sh
@@ -24,16 +24,16 @@ if [ $Model == 'HRNet_Contrast_W48' ]; then
   then
       printf "Debug mode"
       python -m pdb tools/test_egret.py \
-      -c ./configs/image_segmentation/HRNet_Contrast_W48.yaml \
-      -o DataLoader.Eval.dataset.image_root=${CityscapesValRoot}	\
-      -o DataLoader.Eval.dataset.label_path=${CityscapesValLabel} \
+      -c ./configs/image_classification/EfficientNetB0.yaml \
+      -o DataLoader.Eval.dataset.data_root=${ImageNetValRoot}	\
+      -o DataLoader.Eval.dataset.label_path=${ImageNetValLabel} \
       -o DataLoader.Eval.sampler.batch_size=1
   else
       printf "Run mode"
       python tools/test_egret.py \
-      -c ./configs/image_segmentation/HRNet_Contrast_W48.yaml \
-      -o DataLoader.Eval.dataset.image_root=${CityscapesValRoot}	\
-      -o DataLoader.Eval.dataset.label_path=${CityscapesValLabel} \
+      -c ./configs/image_classification/EfficientNetB0.yaml \
+      -o DataLoader.Eval.dataset.data_root=${ImageNetValRoot}	\
+      -o DataLoader.Eval.dataset.label_path=${ImageNetValLabel} \
       -o DataLoader.Eval.sampler.batch_size=1
 
       #-o Global.mode=	\
