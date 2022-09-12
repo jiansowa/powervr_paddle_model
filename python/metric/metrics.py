2bf2c8007dcaae16abb9e313364a5cf29d220a46
jiansowa <james.wjs@hotmail.com>
2022-09-12 21:27:00 +0800

    Add new line after copyright

--- python/metric/metrics.py
+++ python/metric/metrics.py
@@ -10,7 +10,8 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.import numpy as np
+# limitations under the License.
+import numpy as np
 import os
 from PIL import Image
 from sklearn.metrics import top_k_accuracy_score
